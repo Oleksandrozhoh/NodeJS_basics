235436d7787a7baa6d274b5c93a046e3e1fc48a9
Ozhoh, Oleksandr <oleksandrozhoh@gmail.com>
2024-02-22 20:58:42 -0600

    overview nad card html templates, placeholders which will be replaced dynamically once we get responce from data file

--- index.js
+++ index.js
@@ -40,20 +40,56 @@ const url = require("url");
 // console.log("will read file");
 
 ////////////////////////////////////////////////
+// functions
+const replateTemplate = function (template, product) {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = product.organic
+    ? output.replace(/{%NOT_ORGANIC%}/g, "")
+    : output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
 // server
-
+// read all HTML templates
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-// const dataObj = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
+// creating server
 const server = http.createServer((request, response) => {
   console.log(request.url);
   const pathName = request.url;
 
   if (pathName === "/" || pathName === "/overview") {
-    response.end("this is an overview");
+    // OVERVIEW PAGE
+    response.writeHead(200, { "Content-type": "text/html" });
+    const cardsHTML = dataObj
+      .map((element) => replateTemplate(tempCard, element))
+      .join("");
+    const overviewHTML = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
+    response.end(overviewHTML);
   } else if (pathName === "/product") {
+    // PRODUCT PAGE
     response.end("this is the product");
   } else if (pathName === "/api") {
+    // API
     response.writeHead(200, { "Content-type": "application/json" });
     response.end(data);
   } else {
